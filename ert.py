e292be43c7316c6b4048ce4246b47238f93092bb
Vitaliy12323234 <vitalik.stepanov.998@gmail.com>
2023-01-21 17:46:52 +0300

    ''

--- ert.py
+++ ert.py
@@ -26,5 +26,59 @@ for masss in mas:
 
 
 
+# переменная while
+i = 0
+while i < 11:
+    print(i)
+    i = i + 1
+
+
+a = 5
+b = 2
+while(a>5):
+    a-=1
+    b+=a
+print(b)
+
+
+# добавление в таблицу значений
+num = int(input('введите число:'))
+dats = []
+
+while num != 0:
+    dats.append(num)
+    num = int(input('введите число:'))
+dats.sort()
+print(dats)
+
+# добавление в таблицу что бы не повторялись значения
+word = input()
+spis = []
+while word != '':
+    if word not in spis:
+        spis.append(word)
+    word = input()
+for w in spis:
+    print(w)
+
+# определение класса
+class Person:
+# объявлении функции
+    def say_hello(self, message):
+# определяет текущий объект сам себя
+        print(message)
+tom = Person()
+tom.say_hello('hello')
+
+
+
+
+
+
+
+
+
+
+
 
 
